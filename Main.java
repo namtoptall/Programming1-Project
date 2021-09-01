30b5e1dcc73dcfa298076585851f619d7ba6a3eb
namtoptall <73808933+namtoptall@users.noreply.github.com>
2021-09-01 18:43:19 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package finalProject;
 public class Main {
     public static void main(String[] args) {
         userInterface dz = new userInterface();
-        dz.Menu();
+        userInterface.Menu();
     }
 }
+
