6345bbbde905e632bf736077e78be1d4b649cc1d
phanquocthang99 <59633982+phanquocthang99@users.noreply.github.com>
2021-09-02 13:05:33 +0700

    Update display with comments

--- display.java
+++ display.java
@@ -3,12 +3,14 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class display {
+    //Display chart with the chosen metrics by various variables, and arrays.
     public void chartDisplay(long[] data){
         long[] sortedData = bubbleSort(data, "value", "descending");
         long[] sortedIndex = {3,2,4,5,1,6};
         long powerCounter = 1;
         long groupNumber = sortedData.length;
         while (true){
+            //Loop until the powerCounter equal the data that we sorted.
             if (Math.pow(10,powerCounter)>=sortedData[0]){
                 break;
             }else {
@@ -19,21 +21,26 @@ public class display {
         long labelSpaceCount = 0;
         long labelLength = 6;
         long totalLabelSpace = labelSpaceCount*groupNumber+labelLength*groupNumber;
+        //Consider the space of label.
         while (totalLabelSpace<=80){
             labelSpaceCount++;
             totalLabelSpace = labelSpaceCount*groupNumber+labelLength*groupNumber;
         }
         labelSpaceCount--;
+        // Start drawing chart.
         for (int i = 0; i < 23; i++) {
             System.out.print("|");
             long tab_count=0;
             ArrayList<Long> indexInARow = new ArrayList<Long>();
+            // Use loop to scale the chart.
             for (int j = 0; j < sortedData.length; j++) {
                 int rowScale = (int) Math.ceil((23 * sortedData[j]) / (sortedData[0]+Math.pow(10, powerCounter-1)));
+                //Scale the row of the chart.
                 if (rowScale==(23-i)){
                     indexInARow.add(bubbleSort(data, "index", "descending")[j]);
                 }
             }
+            //Create array to take the number of groups.
             long[] indexSort = new long[indexInARow.size()];
             for (int k = 0; k < indexInARow.size(); k++) {
                 indexSort[k] = indexInARow.get(k);
@@ -43,6 +50,7 @@ public class display {
                 System.out.print(" ");
             }
             int starInARow = 0;
+            // Use loop to place the star which demonstrate the value for each group.
             for (int p = 0; p < indexSort.length; p++) {
                 while (tab_count<(indexSort[p]-1)*(labelSpaceCount+labelLength)-starInARow){
                     System.out.print(" ");
@@ -54,10 +62,12 @@ public class display {
             System.out.println();
         }
         System.out.print("|");
+        //Draw horizontal axis.
         for (int q = 0; q < 79; q++) {
             System.out.print("_");
         }
         System.out.println();
+        //Write down the group number.
         for (int i = 1; i <= groupNumber; i+=1) {
             for (int j = 0; j < labelSpaceCount; j++) {
                 System.out.print(" ");
@@ -65,11 +75,16 @@ public class display {
             System.out.print("group"+i);
         }
     }
+    //Display table with the given metrics by various variables and arrays.
     public void tableDisplay(long[] data,ArrayList<ArrayList<Date>> label, String resultType){
         ArrayList<String> labelConversion = new ArrayList<>();
+        //Create a date format.
         SimpleDateFormat obj = new SimpleDateFormat("MM/dd/yy");
+        //Display chart that base on the result type match any of these conditions.
         if (resultType.equals("up_to")){
+            //Display labels.
             for (int i = 0; i < label.size(); i++) {
+                //Initialize label format.
                 String labelFormat = obj.format(label.get(i).get(0))+" - "+obj.format(label.get(i).get(label.get(i).size()-1));
                 labelConversion.add(labelFormat);
             }
@@ -78,6 +93,7 @@ public class display {
             for (long stars : starCount){
                 stars = 0;
             }
+            //Draw table and display variable that depend on the result.
             for (int i = 0; i < labelConversion.size(); i++) {
                 String line = "";
                 line+="*  ";
@@ -86,10 +102,12 @@ public class display {
                 starCount[i]+=line.length();
             }
             long totalStar = bubbleSort(starCount, "value", "descending")[0];
+            //Loop to add star until reach the result value.
             for (int i = 0;i<totalStar;i++){
                 System.out.print("*");
             }
             System.out.println();
+            //Draw table and display variables.
             for (int i = 0; i < labelConversion.size(); i++) {
                 String line = "";
                 System.out.print("*  ");
@@ -97,18 +115,21 @@ public class display {
                 System.out.print(labelConversion.get(i)+"  |  "+String.valueOf(data[i]));
                 line+=labelConversion.get(i)+"  |  "+String.valueOf(data[i]);
                 line+="\t";
-                if (line.length() <totalStar-1){
+                if (line.length() < totalStar-1){
                     for (int j = 0;j<(totalStar-1-line.length());j++){
                         System.out.print(" ");
                     }
                 }
                 System.out.println("*");
             }
+            //Loop to add star until reach the result value.
             for (int i = 0;i<totalStar;i++){
                 System.out.print("*");
             }
             System.out.println();
-        }else if (resultType.equals("new_total")){
+        }
+        //Similar steps to draw table and display variables with another condition.
+        else if (resultType.equals("new_total")){
             String labelFormat = obj.format(label.get(0).get(0))+" - "+obj.format(label.get(label.size()-1).get(label.get(label.size()-1).size()-1));
             labelConversion.add(labelFormat);
             System.out.println();
@@ -156,7 +177,7 @@ public class display {
             for(int i=0; i < n; i++){
                 for(int j=1; j < (n-i); j++){
                     if(sortedArray[j-1] < sortedArray[j]){
-                        //swap elements and index
+                        //Swap elements and index.
                         tempValue = sortedArray[j-1];
                         tempIndex = arrayIndex[j-1];
                         sortedArray[j-1] = sortedArray[j];
@@ -170,7 +191,7 @@ public class display {
             for(int i=0; i < n; i++){
                 for(int j=1; j < (n-i); j++){
                     if(sortedArray[j-1] > sortedArray[j]){
-                        //swap elements and index
+                        //Swap elements and index.
                         tempValue = sortedArray[j-1];
                         tempIndex = arrayIndex[j-1];
                         sortedArray[j-1] = sortedArray[j];
