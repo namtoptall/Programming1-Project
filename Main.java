a9901d11ec2affd333a80db41b29254dc0fd98bc
namtoptall <73808933+namtoptall@users.noreply.github.com>
2021-09-02 08:13:45 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 package finalProject;
-
+// our Main program that pass the value from user interface file
 public class Main {
     public static void main(String[] args) {
         userInterface dz = new userInterface();
